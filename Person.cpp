d99aa80c4b890d637b3c0e52c9bfd6a3b9714b2a
GrawluX <33069541+GrawluX@users.noreply.github.com>
2018-04-19 11:36:44 -0400

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,25 @@
 #include"Person.h"
+
+	Person::Person()
+  {
+    setName("","");
+  }
+	
+	string Person::getName()
+  {
+    return (first + " " + last); 
+  }
+	void Person::setName(string f, string l)
+  {
+    first = f;
+    last = l;
+  }
+
+	string Person::getID()
+  {
+    return (id); 
+  }
+	void Person::setID(string f)
+  {
+    id = f;
+  }
